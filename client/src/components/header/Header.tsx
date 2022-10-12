44412fabd8101972f02777874a0aaf40d69ad4c8
Andy Liu <aliu927@gmail.com>
2022-10-12 00:40:47 -0700

    updated css layout

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import iconEllipsis from "../../assets/icon-vertical-ellipsis.svg";
 
 export default function HeaderTask() {
   return (
     <div className="header_Add_Task_Container">
-      <form action="">
-        <button>Add New Task</button>
-      </form>
+      <h2>Platform Launch</h2>
+      <div className="task_Container">
+        <button className="add_Task">Add New Task</button>
+        <img src={iconEllipsis} alt="" />
+      </div>
     </div>
   );
 }
