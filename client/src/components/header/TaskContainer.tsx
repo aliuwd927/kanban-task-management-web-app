fecfeaee51e2f37319d31c25ce60971ea4ad3ecf
Andy Liu <aliu927@gmail.com>
2022-10-16 15:49:41 -0700

    trying to figure out useContext logic

--- client/src/components/header/TaskContainer.tsx
+++ client/src/components/header/TaskContainer.tsx
@@ -1,6 +1,5 @@
 import BoardEmpty from "../board/BoardEmpty";
 export default function TaskContainer() {
-  console.log(BoardEmpty());
   return (
     <div className="task_Container">
       <BoardEmpty />
--- client/src/components/sidebar/BoardComponent.tsx
+++ client/src/components/sidebar/BoardComponent.tsx
@@ -2,10 +2,12 @@ import { Root } from "../TS Interface JSON/starterInterface";
 export default function BoardComponent() {
   let data: Root = require("../../starter-code/data.json");
   let sideBarData = data.boards;
+  //console.log(sideBarData);
   return (
     <div className="board_Container">
       <div className="all_Boards_Text">All Boards</div>
       {sideBarData.map((element, index: number) => {
+        //console.log(element.columns[0].tasks[0].subtasks);
         return (
           <div key={index} className="board_Button">
             {element.name}
