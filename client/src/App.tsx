aec94d0767a03d40e2a7d0f56ba2476ffcda3c7a
Andy Liu <aliu927@gmail.com>
2022-10-16 00:57:58 -0700

    trying to figure out add board logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./App.css";
 import HeaderTask from "./components/header/Header";
 import TaskContainer from "./components/header/TaskContainer";
--- client/src/components/header/TaskContainer.tsx
+++ client/src/components/header/TaskContainer.tsx
@@ -1,5 +1,6 @@
 import BoardEmpty from "../board/BoardEmpty";
 export default function TaskContainer() {
+  console.log(BoardEmpty());
   return (
     <div className="task_Container">
       <BoardEmpty />
