161e0d0bc420c8e53ee86b954541fcfd541bec0d
Andy Liu <aliu927@gmail.com>
2022-11-08 23:05:58 -0800

    figured out subTasks Length

--- client/src/components/board/TaskComponent.tsx
+++ client/src/components/board/TaskComponent.tsx
@@ -12,39 +12,26 @@ export default function TaskComponent() {
         return element?.columns;
       }) ?? [];
 
-  let subTasks = colArr?.map((element) => {
-    let subArr = element?.tasks?.map((element) => {
-      let lengthOfTask = element?.subtasks?.length;
-      return lengthOfTask;
-    });
-    return subArr;
-  });
+
 
   let displayTaskArray = colArr?.map((element) => {
     let taskArray = element?.tasks?.map((element) => {
-      return <div className="task_Title">{element.title}</div>;
+      let subTasks = element?.subtasks?.length;
+      return (
+      <div className="task_Title">
+        {element.title}
+        <div>{subTasks}</div>
+        </div>
+      );
     });
     return (
       <div className="inner_Task_Column_Container">
         <div className={`status_Name_Container`}>{element.name}</div>
         <div>{taskArray}</div>
-        <div>{subTasks}</div>
       </div>
     );
   });
 
-  //Put this map within subtasks.
-
-  console.log(subTasks);
-  //console.log(subLength);
   return <div className="task_Column">{displayTaskArray}</div>;
 }
 
-{
-  /*
-
-tutschei: Make a Component for the Card
-tutschei: And pass the length in
-
-*/
-}
