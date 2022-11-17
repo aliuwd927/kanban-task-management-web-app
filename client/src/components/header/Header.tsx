b0a625bd456474bd91680eec1a1827d894b14845
Andy Liu <aliu927@gmail.com>
2022-11-16 23:56:14 -0800

    seperated menu component headless ui

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,27 +1,15 @@
-import { Fragment } from "react";
 import iconEllipsis from "../../assets/icon-vertical-ellipsis.svg";
-import { Menu } from "@headlessui/react";
+import MenuAddTask from "./MenuAddTask";
 
-const links = [{ label: "Edit Board" }, { label: "Delete Board" }];
+
+
 
 export default function HeaderTask() {
   return (
     <div className="header_Add_Task_Container">
       <h2>Platform Launch</h2>
       <div className="add_Task_Container_Container">
-        <Menu>
-          <div className="add_task_Container">
-            <Menu.Button className="add_Task">Add New Task</Menu.Button>
-
-            <Menu.Items className="list_label_Container">
-              {links.map((link) => (
-                <Menu.Item as="div" className="list_label">
-                  {link.label}
-                </Menu.Item>
-              ))}
-            </Menu.Items>
-          </div>
-        </Menu>
+        <MenuAddTask/>
         <div className="icon_Ellipsis_Container">
           <img src={iconEllipsis} alt="" />
         </div>
