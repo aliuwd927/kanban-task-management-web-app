2f112dc45c056cb1678281ff1b7c58e6449e5670
Andy Liu <aliu927@gmail.com>
2022-11-21 21:59:48 -0800

    chaning theme comp to headlessui

--- client/src/components/sidebar/ThemeComponent.tsx
+++ client/src/components/sidebar/ThemeComponent.tsx
@@ -13,3 +13,24 @@ export default function ThemeComponent() {
     </div>
   );
 }
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * export default function ThemeComponent() {
+  return (
+    <div className="theme_Container">
+      <img src={iconlighttheme} alt="" className="icon_Light_Theme_Img" />
+      <div className="overlay"></div>
+      <input className="slide_Toggle" id={`react-switch-new`} type="checkbox" />
+      <label className="switch_Label" htmlFor={`react-switch-new`}>
+        <span className={`switch_Button`} />
+      </label>
+      <img src={icondarktheme} alt="" className="icon_Dark_Theme_Img" />
+    </div>
+  );
+}
+
+ */
