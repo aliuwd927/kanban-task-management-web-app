8c847c1f80438316373de4f07cfc650de7a1045c
Andy Liu <aliu927@gmail.com>
2022-11-26 23:00:50 -0800

    fixed function name

--- client/src/components/header/MenuAddTask.tsx
+++ client/src/components/header/MenuAddTask.tsx
@@ -1,21 +1,21 @@
-import {Menu} from "@headlessui/react"
+import { Menu } from "@headlessui/react";
 
 const links = [{ label: "Edit Board" }, { label: "Delete Board" }];
 
-export default function(){
-    return(
-        <Menu>
-        <div className="add_task_Container">
-          <Menu.Button className="add_Task">Add New Task</Menu.Button>
+export default function MenuAddTask() {
+  return (
+    <Menu>
+      <div className="add_task_Container">
+        <Menu.Button className="add_Task">Add New Task</Menu.Button>
 
-          <Menu.Items className="list_label_Container">
-            {links.map((link) => (
-              <Menu.Item as="div" className="list_label">
-                {link.label}
-              </Menu.Item>
-            ))}
-          </Menu.Items>
-        </div>
-      </Menu>
-    )
+        <Menu.Items className="list_label_Container">
+          {links.map((link) => (
+            <Menu.Item as="div" className="list_label">
+              {link.label}
+            </Menu.Item>
+          ))}
+        </Menu.Items>
+      </div>
+    </Menu>
+  );
 }
